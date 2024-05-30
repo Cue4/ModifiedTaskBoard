3d298842e82c20384c6bfc7093c21f33ffb0d442
Caliph Ferguson <Quasar@Caliphs-Mini.lan>
2024-05-30 15:10:06 -0400

    added new task card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,49 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+let taskList = JSON.parse(localStorage.getItem("tasks"))|| [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    return `task-${Date.now()}`
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const taskCard = $('<div>')
+        .addClass('card task-card draggable my-3')
+        .attr('data-task-id', task.id);
+    const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
+    const cardBody = $('<div>').addClass('card-body');
+    const cardDescription = $('<p>').addClass('card-text').text(task.type);
+    const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+    const cardDeleteBtn = $('<button>')
+        .addClass('btn btn-danger delete')
+        .text('Delete')
+        .attr('data-task-id', task.id);
+    cardDeleteBtn.on('click', handleDeleteTask);
 
+if (task.dueDate && task.status !== 'done') {
+    const now = dayjs();
+    const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+
+  if (now.isSame(taskDueDate, 'day')) {
+    taskCard.addClass('bg-warning text-white');
+  } 
+  else if (now.isAfter(taskDueDate)) {
+    taskCard.addClass('bg-danger text-white');
+    cardDeleteBtn.addClass('border-light');
+  }
+}
+cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+taskCard.append(cardHeader, cardBody);
+
+return taskCard;
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+    const taskListContainer = document.getElementById('taskList');
+    const taskCard = renderTaskList(task.id, task.name, task.description)
 }
 
 // Todo: create a function to handle adding a new task
